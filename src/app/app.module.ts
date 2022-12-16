3c478556b1d96af9c5b40d63c760a20e826ebb33
hertonmota <metro.mota@gmail.com>
2022-12-16 07:47:05 -0300

    feat(core): adição do httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { HomeComponent } from "./pages/home/home.component";
-import { TableProductsComponent } from './shared/components/table-products/table-products.component'
+import { HomeComponent } from "./pages/home/home.component"
+import { TableProductsComponent } from "./shared/components/table-products/table-products.component"
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, TableProductsComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
