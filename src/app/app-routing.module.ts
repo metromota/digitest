1995915e08eac9311bd740b10d9c4043d57ae899
hertonmota <metro.mota@gmail.com>
2022-12-16 07:29:29 -0300

    refactor(app): refatoração do código

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { homeRoutes } from './pages/home/home.routes'
+import { NgModule } from "@angular/core"
+import { RouterModule, Routes } from "@angular/router"
+import { homeRoutes } from "./pages/home/home.routes"
 
 const routes: Routes = [...homeRoutes]
 
--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router'
-import { HomeComponent } from './home.component'
+import { Routes } from "@angular/router"
+import { HomeComponent } from "./home.component"
 
 const homeRoutes: Routes = [
     {
-        path: '',
+        path: "",
         component: HomeComponent,
     },
 ]
