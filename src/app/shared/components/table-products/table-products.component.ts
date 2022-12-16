53e034a733f7ba0b83e318725090cd2cd8aaebaa
hertonmota <metro.mota@gmail.com>
2022-12-16 18:53:32 -0300

    refactor(shared): adicionado a tabela do angular material

--- src/app/shared/components/table-products/table-products.component.ts
+++ src/app/shared/components/table-products/table-products.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { Product } from "../../models/product"
 
 @Component({
-  selector: 'app-table-products',
-  templateUrl: './table-products.component.html',
-  styleUrls: ['./table-products.component.css']
+    selector: "app-table-products",
+    templateUrl: "./table-products.component.html",
+    styleUrls: ["./table-products.component.css"],
 })
 export class TableProductsComponent {
-
+    public productList$: Product[] = []
+    public displayedColumns: string[] = ["id", "title", "price", "brand"]
 }
