4b587ee3509ab09955523bedf26a4f8a12c96444
hertonmota <metro.mota@gmail.com>
2022-12-16 07:39:00 -0300

    feat(shared): criação do componente tabela de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from "@angular/platform-browser"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { HomeComponent } from "./pages/home/home.component"
+import { HomeComponent } from "./pages/home/home.component";
+import { TableProductsComponent } from './shared/components/table-products/table-products.component'
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent],
+    declarations: [AppComponent, HomeComponent, TableProductsComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/shared/components/table-products/table-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-products',
+  templateUrl: './table-products.component.html',
+  styleUrls: ['./table-products.component.css']
+})
+export class TableProductsComponent {
+
+}
