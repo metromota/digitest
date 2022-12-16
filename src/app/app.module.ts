f0f7703e33dc732f72acf05004c85810c2b5ae80
hertonmota <metro.mota@gmail.com>
2022-12-16 18:47:23 -0300

    feat(app): adicionado tabela angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { HttpClientModule } from "@angular/common/http"
+import { MatTableModule } from "@angular/material/table"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
@@ -15,6 +16,7 @@ import { TableProductsComponent } from "./shared/components/table-products/table
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
+        MatTableModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
