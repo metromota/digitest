3596f3ddd19179bef176c4d4fe06f5c4f31494b8
hertonmota <metro.mota@gmail.com>
2022-12-16 19:33:05 -0300

    refactor(core): adicionando Environment em ProductService

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
+import { Environment } from "src/app/shared/environment"
 
 @Injectable({
     providedIn: "root",
 })
 export class ProductService {
     constructor(private http: HttpClient) {}
+
+    listProducts() {
+        return this.http.get<any>(Environment.getDomain())
+    }
 }
