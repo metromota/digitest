520a0d0446a3188954b54b91bc58eaa9d9d012c3
hertonmota <metro.mota@gmail.com>
2022-12-17 13:53:34 -0300

    feat(shared): adicionado paginator

--- src/app/shared/components/table-products/table-products.component.ts
+++ src/app/shared/components/table-products/table-products.component.ts
@@ -9,18 +9,46 @@ import { MatPaginator } from "@angular/material/paginator"
     templateUrl: "./table-products.component.html",
 })
 export class TableProductsComponent implements OnInit {
-    displayedColumns: string[] = ["id", "title", "price", "brand"]
+    pageSize: number
+    pageIndex: number
+    length: number
+    displayedColumns: string[] = [
+        "id",
+        "title",
+        "description",
+        "price",
+        "brand",
+        "edit",
+    ]
     dataSource: MatTableDataSource<Product>
     @ViewChild(MatPaginator) paginator: MatPaginator
 
     constructor(private service: ProductService) {}
 
     ngOnInit(): void {
-        this.service.listProducts().subscribe((dummyApiAllProducts) => {
-            this.dataSource = new MatTableDataSource<Product>(
-                dummyApiAllProducts.products
-            )
-            this.dataSource.paginator = this.paginator
-        })
+        this.service
+            .listPaginatedProducts(5, 0)
+            .subscribe((productsFromApi) => {
+                let sizeLength = productsFromApi.total
+                this.dataSource = new MatTableDataSource<Product>(
+                    productsFromApi.products
+                )
+                this.dataSource.paginator = this.paginator
+                this.length = sizeLength
+
+                console.log(sizeLength)
+            })
+    }
+
+    handlePageEvent($event) {
+        let limit = $event.pageSize
+        let skip = $event.pageIndex * $event.pageSize
+        this.service
+            .listPaginatedProducts(limit, skip)
+            .subscribe((productsFromApi) => {
+                this.dataSource = new MatTableDataSource<Product>(
+                    productsFromApi.products
+                )
+            })
     }
 }
