e7bedf7a6903060b29d83fff6b2ec9ef89109822
hertonmota <metro.mota@gmail.com>
2022-12-17 13:52:15 -0300

    refactor(app): organização das importações de módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import { HttpClientModule } from "@angular/common/http"
 import { MatTableModule } from "@angular/material/table"
 import { MatPaginatorModule } from "@angular/material/paginator"
 import { MatDialogModule } from "@angular/material/dialog"
+import { MatButtonModule } from "@angular/material/button"
+import { MatIconModule } from "@angular/material/icon"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { HomeComponent } from "./pages/home/home.component"
-import { TableProductsComponent } from "./shared/components/table-products/table-products.component";
-import { DialogWindowComponent } from './shared/components/dialog/dialog.window/dialog.window.component';
-import { SaveFormComponent } from './shared/components/dialog/save.form/save.form.component';
-import { UpdateFormComponent } from './shared/components/dialog/update.form/update.form.component'
+import { TableProductsComponent } from "./shared/components/table-products/table-products.component"
+import { DialogSaveProductComponent } from "./shared/components/dialog-save-product/dialog-save-product.component"
+import { DialogSaveProductContentComponent } from "./shared/components/dialog-save-product-content/dialog-save-product-content.component"
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, TableProductsComponent, DialogWindowComponent, SaveFormComponent, UpdateFormComponent],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        TableProductsComponent,
+        DialogSaveProductComponent,
+        DialogSaveProductContentComponent,
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -24,6 +31,8 @@ import { UpdateFormComponent } from './shared/components/dialog/update.form/upda
         MatTableModule,
         MatPaginatorModule,
         MatDialogModule,
+        MatButtonModule,
+        MatIconModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
