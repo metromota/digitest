0383666eef54737b08b12b34d858412a4ef1ba49
hertonmota <metro.mota@gmail.com>
2022-12-17 21:59:25 -0300

    refactor(shared): refatorado o codigo do componente table product

--- src/app/shared/components/table-products/table-products.component.ts
+++ src/app/shared/components/table-products/table-products.component.ts
@@ -9,43 +9,47 @@ import { MatPaginator } from "@angular/material/paginator"
     templateUrl: "./table-products.component.html",
 })
 export class TableProductsComponent implements OnInit {
+    loading: boolean
     pageSize: number
     pageIndex: number
     length: number
     displayedColumns: string[] = [
         "id",
+        "edit",
         "title",
         "description",
-        "price",
         "brand",
-        "edit",
+        "price",
     ]
     dataSource: MatTableDataSource<Product>
     @ViewChild(MatPaginator) paginator: MatPaginator
 
-    constructor(private service: ProductService) {}
+    constructor(private service: ProductService) {
+        this.loading = true
+    }
 
     ngOnInit(): void {
         this.service
             .listPaginatedProducts(5, 0)
             .subscribe((productsFromApi) => {
-                let sizeLength = productsFromApi.total
+                const sizeLength = productsFromApi.total
+                this.loading = false
                 this.dataSource = new MatTableDataSource<Product>(
                     productsFromApi.products
                 )
                 this.dataSource.paginator = this.paginator
                 this.length = sizeLength
-
-                console.log(sizeLength)
             })
     }
 
     handlePageEvent($event) {
-        let limit = $event.pageSize
-        let skip = $event.pageIndex * $event.pageSize
+        this.loading = true
+        const limit = $event.pageSize
+        const skip = $event.pageIndex * $event.pageSize
         this.service
             .listPaginatedProducts(limit, skip)
             .subscribe((productsFromApi) => {
+                this.loading = false
                 this.dataSource = new MatTableDataSource<Product>(
                     productsFromApi.products
                 )
