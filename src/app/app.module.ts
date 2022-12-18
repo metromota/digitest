49e5ad771630a80309fef9ac321c38b0661307bc
hertonmota <metro.mota@gmail.com>
2022-12-18 20:56:50 -0300

    refactor(app): refatoração para legibilidade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
+// modulso angular
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { HttpClientModule } from "@angular/common/http"
 import { ReactiveFormsModule } from "@angular/forms"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { AppRoutingModule } from "./app-routing.module"
-
-import { MatTableModule } from "@angular/material/table"
+// Modulos material
 import {
     MatPaginatorIntl,
     MatPaginatorModule,
 } from "@angular/material/paginator"
+import { MatTableModule } from "@angular/material/table"
 import { MatDialogModule } from "@angular/material/dialog"
 import { MatButtonModule } from "@angular/material/button"
 import { MatIconModule } from "@angular/material/icon"
 import { MatDividerModule } from "@angular/material/divider"
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
-
+// modulos do scafold e criados
+import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { HomeComponent } from "./pages/home/home.component"
 import { TableProductsComponent } from "./shared/components/table-products/table-products.component"
@@ -25,7 +26,7 @@ import { LoadingComponent } from "./shared/components/loading/loading.component"
 import { DialogUpdateProductComponent } from "./shared/components/dialog-update-product/dialog-update-product.component"
 import { DialogUpdateProductContentComponent } from "./shared/components/dialog-update-product-content/dialog-update-product-content.component"
 import { MatPaginatorIntlPtBr } from "./shared/intl/paginator.ptbr"
-
+// modulos de terceiros
 import { ToastrModule } from "ngx-toastr"
 
 @NgModule({
